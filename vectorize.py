467d67f0bf83af8bfda1cb29eeebc9d359c2bb68
oscarzwagers <o.zwagers@icloud.com>
2024-01-28 00:19:26 +0100

    the new feature 'sentence-similarity' can now be vectorized. Also the requirement for trigrams of token based features to be taken into account is stricter.

--- vectorize.py
+++ vectorize.py
@@ -1,5 +1,7 @@
 import json
 import argparse
+import statistics
+
 from tqdm import tqdm
 from utilities import Features
 from typing import Any, Literal
@@ -9,6 +11,11 @@ from enum import Enum
 import numpy as np
 import os
 import pickle
+
+"""
+The features that are extracted from the data should be turned into vectors so that classifiers and neural networks can
+be trained on them. This file contains the code to vectorize the data.
+"""
 
 TOKEN_BASED_FEATURES = {
     Features.DEP_TAGS,
@@ -21,10 +28,12 @@ SENTENCE_BASED_FEATURES = {
     Features.VOICE,
 }
 
-DOCUMENT_BASED_FEATURES = {
-    Features.NAMED_ENTITIES,
-    Features.PRONOUNS,
-    Features.DOMAIN,
+FEATURES_WITH_VECTORIZER = {
+    Features.DEP_TAGS,
+    Features.POS_TAGS,
+    Features.SENTIMENT,
+    Features.TENSE,
+    Features.VOICE,
 }
 
 DOMAINS = {
@@ -109,10 +118,10 @@ def add_arguments(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
 
 
 def vectorize_token_based_features(
-    data: dict[int, Any], feature: Features, options: VectorizeOptions
+        data: dict[int, Any], feature: Features, options: VectorizeOptions
 ) -> dict[Literal["vectorizer", "vectors"], Any]:
     ngram_range = (options.token_N_grams, options.token_N_grams)
-    min_df = 25 if feature.DEP_TAGS else 5
+    min_df = 0.25 if (feature == Features.POS_TAGS or feature == Features.DEP_TAGS) else 5
 
     values = [" ".join(item) for item in data.values()]
 
@@ -131,7 +140,7 @@ def vectorize_token_based_features(
 
 
 def vectorize_sentence_based_features(
-    data: dict[int, Any], feature: Features, options: VectorizeOptions
+        data: dict[int, Any], feature: Features, options: VectorizeOptions
 ) -> dict[Literal["vectorizer", "vectors"], Any]:
     ngram_range = (options.sentence_N_grams, options.sentence_N_grams)
 
@@ -151,8 +160,46 @@ def vectorize_sentence_based_features(
     return {"vectorizer": vectorizer, "vectors": vector}
 
 
+def vectorize_sentence_similarity(
+        data: dict[int, list[tuple[float, float]]], options: VectorizeOptions
+) -> dict[Literal["vectors"], Any]:
+    values = []
+
+    for id, similarities in data.items():
+        min_prev_similarity = 1
+        max_prev_similarity = -1
+        min_next_similarity = 1
+        max_next_similarity = -1
+        mean_prev_similarity = []
+        mean_next_similarity = []
+        amount_next_similarity_higher = 0
+
+        for index, (prev_similarity, next_similarity) in enumerate(similarities):
+            min_prev_similarity = min(min_prev_similarity, prev_similarity)
+            max_prev_similarity = max(max_prev_similarity, prev_similarity)
+            min_next_similarity = min(min_next_similarity, next_similarity)
+            max_next_similarity = max(max_next_similarity, next_similarity)
+            mean_prev_similarity.append(prev_similarity)
+            mean_next_similarity.append(next_similarity)
+
+            if next_similarity > prev_similarity:
+                amount_next_similarity_higher += 1
+
+        values.append([
+            min_prev_similarity,
+            max_prev_similarity,
+            min_next_similarity,
+            max_next_similarity,
+            statistics.mean(mean_prev_similarity),
+            statistics.mean(mean_next_similarity),
+            amount_next_similarity_higher / len(similarities)
+        ])
+
+    return {"vectors": np.array(values)}
+
+
 def vectorize_data(
-    data: dict[int, Any], feature: Features, options: VectorizeOptions
+        data: dict[int, Any], feature: Features, options: VectorizeOptions
 ) -> dict[Literal["vectorizer", "vectors"], Any]:
     match feature:
         case feature if feature in TOKEN_BASED_FEATURES:
@@ -163,12 +210,14 @@ def vectorize_data(
             return {"vectors": np.array([[item] for item in data.values()])}
         case Features.DOMAIN:
             return {"vectors": np.array([[DOMAINS[item]] for item in data.values()])}
+        case Features.SENTENCE_SIMILARITY:
+            return vectorize_sentence_similarity(data, options)
         case default:
             return dict()
 
 
 def save_vector_data(
-    result: dict[Literal["vectorizer", "vectors"], Any], output: str, feature: Features
+        result: dict[Literal["vectorizer", "vectors"], Any], output: str, feature: Features
 ):
     dirname = os.path.join(output, feature.value)
     os.makedirs(dirname, exist_ok=True)
@@ -191,7 +240,7 @@ def main():
     vectorizers = {}
 
     if arguments.vectorizer:
-        for feature in features:
+        for feature in FEATURES_WITH_VECTORIZER.intersection(set(features)):
             filename = os.path.join(
                 arguments.vectorizer, feature.value, "vectorizer.pkl"
             )
